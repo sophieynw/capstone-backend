12c995a38c681e0d44a25a8eef12f5a27c0188f7
Sophie Wang <wang.sophie@live.ca>
2026-07-31 14:24:21 -0400

    added more properties & checklist items, organized a bit

--- restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/bootstrap/BootstrapData.java
+++ restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/bootstrap/BootstrapData.java
@@ -1,7 +1,6 @@
 package ca.sheridancollege.restfulhousekeeping.bootstrap;
 
 import java.time.LocalDate;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
@@ -44,29 +43,212 @@ public class BootstrapData implements CommandLineRunner {
 	private final CleaningChecklistItemRepository cleaningChecklistItemRepository;
 	private final SlotRepository slotRepository;
 	private final PasswordEncoder passwordEncoder;
+	
+	// the following two functions can be used to generate a "random" completedDaysAgo number from 1 to 60 days
+	
+	private int randomInt(int min, int max) {
+	    return ThreadLocalRandom.current()
+	        .nextInt(min, max + 1);
+	}
+	
+	private ChecklistItem generateChecklistItem(Property property, String description, Integer frequencyDays, Integer completedDaysAgo) {
+		return ChecklistItem.builder()
+				.property(property)
+				.description(description)
+				.frequencyDays(frequencyDays)
+				.lastCompleted(LocalDate.now().minusDays(randomInt(1, 60)).atStartOfDay())
+				.build();
+	}
+		
+//	private ChecklistItem generateChecklistItem(Property property, String description, Integer frequencyDays, Integer completedDaysAgo) {
+//		return ChecklistItem.builder()
+//				.property(property)
+//				.description(description)
+//				.frequencyDays(frequencyDays)
+//				.lastCompleted(LocalDate.now().minusDays(completedDaysAgo).atStartOfDay())
+//				.build();
+//	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		if (organizationRepository.count() > 0) {
 			return;
 		}
-
+		
+		
+		
+		// Organizations
 		Organization organization1 = Organization.builder().name("The Everything Company")
 				.description("We specialize in literally everything.").build();
-		User manager1 = User.builder().firstName("Sophie").lastName("Wang").username("sophie").email("sophie@test.com")
-				.password(passwordEncoder.encode("password")).phoneNumber("6471231234").role(Role.MANAGER).build();
-		User cleaner1 = User.builder().firstName("Robert").lastName("Fleming").username("robert")
-				.email("robert@test.com").password(passwordEncoder.encode("password")).phoneNumber("9056083833")
-				.role(Role.CLEANER).build();
-		User cleaner2 = User.builder().firstName("Katie").lastName("McEwan").username("katie").email("katie@test.com")
-				.password(passwordEncoder.encode("password")).phoneNumber("2262240336").role(Role.CLEANER).build();
-		Property property1 = Property.builder().name("Wayward Pines").street("1234").unit("123").city("Mississauga")
-				.province("Ontario").postalCode("L6D 7N4").country("Canada")
-				.accessInstructions("Door locked. Use key under mat.").build();
-		Property property2 = Property.builder().name("Union St.").street("101").unit("292").city("Oakville")
-				.province("Ontario").postalCode("L4V 1V1").country("Canada")
-				.accessInstructions("Jump over the fence and take a right.").build();
+		organization1 = organizationRepository.save(organization1);
+		
+		
+		
+		// People
+		User manager1 = User.builder()
+				.organization(organization1)
+				.firstName("Sophie")
+				.lastName("Wang")
+				.username("sophie")
+				.email("sophie@test.com")
+				.password(passwordEncoder.encode("password"))
+				.phoneNumber("6471231234")
+				.role(Role.MANAGER)
+				.build();
+		User cleaner1 = User.builder()
+				.organization(organization1)
+				.firstName("Robert")
+				.lastName("Fleming")
+				.username("robert")
+				.email("robert@test.com")
+				.password(passwordEncoder.encode("password"))
+				.phoneNumber("9056083833")
+				.role(Role.CLEANER)
+				.build();
+		User cleaner2 = User.builder()
+				.organization(organization1)
+				.firstName("Katie")
+				.lastName("McEwan")
+				.username("katie")
+				.email("katie@test.com")
+				.password(passwordEncoder.encode("password"))
+				.phoneNumber("2260001234")
+				.role(Role.CLEANER)
+				.build();
+		manager1 = userRepository.save(manager1);
+		cleaner1 = userRepository.save(cleaner1);
+		cleaner2 = userRepository.save(cleaner2);
+		
+		
+		
+		// Sophie's Properties
+		Property property1 = Property.builder()
+				.manager(manager1)
+			    .name("Wayward Pines House")
+			    .street("1234 Wayward Pines")
+			    .unit("123")
+			    .city("Mississauga")
+			    .province("Ontario")
+			    .postalCode("L6D 7N4")
+			    .country("Canada")
+			    .accessInstructions("Door locked. Use key under mat.")
+			    .build();
+		Property property2 = Property.builder()
+				.manager(manager1)
+			    .name("Union Condo")
+			    .street("101 Union St.")
+			    .unit("292")
+			    .city("Oakville")
+			    .province("Ontario")
+			    .postalCode("L4V 1V1")
+			    .country("Canada")
+			    .accessInstructions(
+			        "Jump over the fence and take a right."
+			    )
+			    .build();
+		Property property3 = Property.builder()
+				.manager(manager1)
+			    .name("Maple Ridge House")
+			    .street("47 Maple Ridge Drive")
+			    .city("Burlington")
+			    .province("Ontario")
+			    .postalCode("L7M 2P8")
+			    .country("Canada")
+			    .accessInstructions(
+			        "Use the side entrance. Lockbox is attached to the railing."
+			    )
+			    .build();
+		Property property4 = Property.builder()
+				.manager(manager1)
+			    .name("Harbourview Condo")
+			    .street("2500 Lakeshore Road West")
+			    .unit("604")
+			    .city("Oakville")
+			    .province("Ontario")
+			    .postalCode("L6L 1H8")
+			    .country("Canada")
+			    .accessInstructions(
+			        "Check in with the concierge and ask for the unit key."
+			    )
+			    .build();
+		Property property5 = Property.builder()
+				.manager(manager1)
+			    .name("Downtown Loft")
+			    .street("80 King Street West")
+			    .unit("1205")
+			    .city("Toronto")
+			    .province("Ontario")
+			    .postalCode("M5H 1J9")
+			    .country("Canada")
+			    .accessInstructions(
+			        "Enter through the west lobby. The access code is in the booking notes."
+			    )
+			    .build();
+		property1 = propertyRepository.save(property1);
+		property2 = propertyRepository.save(property2);
+		property3 = propertyRepository.save(property3);
+		property4 = propertyRepository.save(property4);
+		property5 = propertyRepository.save(property5);
+		
+		
+		
+		// Checklist Items
+		// Property 1 checklist items
+		ChecklistItem kitchen = generateChecklistItem(property1, "Clean kitchen counters and sink", 1, 1);
+		ChecklistItem bathroom = generateChecklistItem(property1, "Clean and disinfect bathroom", 1, 3);
+		ChecklistItem bedding = generateChecklistItem(property1, "Change bed linens", 1, 7);
+		ChecklistItem supplies = generateChecklistItem(property1, "Check if supplies need to be replaced", 15, 4);
+		ChecklistItem furnaceFilter = generateChecklistItem(property1, "Replace furnace filter", 45, 60);
+		ChecklistItem ceilingCorners = generateChecklistItem(property1, "Dust ceiling corners", 15, 5);
+		ChecklistItem baseboards = generateChecklistItem(property1, "Clean baseboards", 30, 35);
+		checklistItemRepository
+				.saveAll(List.of(kitchen, bathroom, bedding, supplies, furnaceFilter, ceilingCorners, baseboards));
 
+		// Property 2 checklist items
+		ChecklistItem kitchen2 = generateChecklistItem(property2, "Clean kitchen counters and sink", 1, 2);
+		ChecklistItem bathroom2 = generateChecklistItem(property2, "Clean and disinfect bathroom", 1, 2);
+		ChecklistItem bedding2 = generateChecklistItem(property2, "Change bed linens", 1, 6);
+		ChecklistItem supplies2 = generateChecklistItem(property2, "Check if supplies need to be replaced", 20, 4);
+		ChecklistItem hottub = generateChecklistItem(property2, "Check water level in hot tub", 7, 7);
+		ChecklistItem windows2 = generateChecklistItem(property2, "Clean windows and balcony doors", 15, 3);
+		ChecklistItem vents2 = generateChecklistItem(property2, "Dust vents and air returns", 45, 5);
+		checklistItemRepository.saveAll(List.of(kitchen2, bathroom2, bedding2, supplies2, hottub, windows2, vents2));
+
+		// Property 3 checklist items
+		ChecklistItem floors3 = generateChecklistItem(property3, "Vacuum carpets and mop floors", 1, 1);
+		ChecklistItem bathroom3 = generateChecklistItem(property3, "Clean and disinfect bathrooms", 1, 4);
+		ChecklistItem bedding3 = generateChecklistItem(property3, "Change bed linens", 1, 5);
+		ChecklistItem ceilingFans3 = generateChecklistItem(property3, "Dust ceiling fans", 15, 6);
+		ChecklistItem furnaceFilter3 = generateChecklistItem(property3, "Replace furnace filter", 45, 7);
+		ChecklistItem windows3 = generateChecklistItem(property3, "Clean interior windows", 20, 3);
+		ChecklistItem sinks3 = generateChecklistItem(property3, "Check under sinks for leaks", 15, 2);
+		checklistItemRepository
+				.saveAll(List.of(floors3, bathroom3, bedding3, ceilingFans3, furnaceFilter3, windows3, sinks3));
+
+		// Property 4 checklist items
+		ChecklistItem kitchen4 = generateChecklistItem(property4, "Clean kitchen surfaces", 1, 1);
+		ChecklistItem bathroom4 = generateChecklistItem(property4, "Clean bathroom fixtures", 1, 3);
+		ChecklistItem towels4 = generateChecklistItem(property4, "Replace towels and linens", 1, 4);
+		ChecklistItem appliances4 = generateChecklistItem(property4, "Wipe appliance exteriors", 7, 5);
+		ChecklistItem windows4 = generateChecklistItem(property4, "Clean windows and mirrors", 14, 6);
+		ChecklistItem balcony4 = generateChecklistItem(property4, "Sweep and wash balcony", 30, 7);
+		ChecklistItem smokeDetectors4 = generateChecklistItem(property4, "Test smoke detectors", 60, 2);
+
+		checklistItemRepository
+				.saveAll(List.of(kitchen4, bathroom4, towels4, appliances4, windows4, balcony4, smokeDetectors4));
+
+		// Property 5 checklist items
+		ChecklistItem floors5 = generateChecklistItem(property5, "Sweep and mop floors", 1, 2);
+		ChecklistItem shower5 = generateChecklistItem(property5, "Clean glass shower doors", 1, 1);
+		ChecklistItem surfaces5 = generateChecklistItem(property5, "Wipe counters and surfaces", 1, 4);
+		ChecklistItem shelves5 = generateChecklistItem(property5, "Dust shelves and exposed pipes", 7, 6);
+		ChecklistItem rangeHood5 = generateChecklistItem(property5, "Clean range hood filter", 30, 7);
+		ChecklistItem sofa5 = generateChecklistItem(property5, "Vacuum sofa and cushions", 14, 5);
+		ChecklistItem alarms5 = generateChecklistItem(property5, "Test smoke and carbon monoxide alarms", 60, 3);
+		checklistItemRepository.saveAll(List.of(floors5, shower5, surfaces5, shelves5, rangeHood5, sofa5, alarms5));	
+		
+
+		
 		// Cleanings
 		Cleaning cleaning1 = Cleaning.builder().dateTimeStart(LocalDateTime.of(2026, 7, 21, 8, 30))
 				.dateTimeEnd(LocalDateTime.of(2026, 7, 21, 12, 30))
@@ -84,24 +266,6 @@ public class BootstrapData implements CommandLineRunner {
 				.endTime(LocalTime.of(17, 0)).build();
 		AvailabilitySlot slot2 = AvailabilitySlot.builder().dayOfWeek(DayOfWeek.WEDNESDAY).startTime(LocalTime.of(9, 0))
 				.endTime(LocalTime.of(17, 0)).build();
-
-		organization1 = organizationRepository.save(organization1);
-
-		// Managers
-		manager1.setOrganization(organization1);
-		manager1 = userRepository.save(manager1);
-
-		// Cleaners
-		cleaner1.setOrganization(organization1);
-		cleaner1 = userRepository.save(cleaner1);
-		cleaner2.setOrganization(organization1);
-		cleaner2 = userRepository.save(cleaner2);
-
-		// Properties
-		property1.setManager(manager1); // sophie's property
-		property1 = propertyRepository.save(property1);
-		property2.setManager(manager1);
-		property2 = propertyRepository.save(property2);
 
 		// Cleanings
 		cleaning1.setProperty(property1);
@@ -121,29 +285,6 @@ public class BootstrapData implements CommandLineRunner {
 		cleaning4.setManager(manager1);
 		cleaning4 = cleaningRepository.save(cleaning4);
 
-		// Property 1 checklist items
-		ChecklistItem kitchen = ChecklistItem.builder().property(property1)
-				.description("Clean kitchen counters and sink").frequencyDays(1).build();
-		ChecklistItem bathroom = ChecklistItem.builder().property(property1).description("Clean and disinfect bathroom")
-				.frequencyDays(1).build();
-		ChecklistItem bedding = ChecklistItem.builder().property(property1).description("Change bed linens")
-				.frequencyDays(1).build();
-		ChecklistItem supplies = ChecklistItem.builder().property(property1)
-				.description("Check if any supplies need to be replaced").frequencyDays(15).build();
-		checklistItemRepository.saveAll(List.of(kitchen, bathroom, bedding, supplies));
-
-		// Property 2 checklist items
-		ChecklistItem kitchen2 = ChecklistItem.builder().property(property2)
-				.description("Clean kitchen counters and sink").frequencyDays(1).build();
-		ChecklistItem bathroom2 = ChecklistItem.builder().property(property2)
-				.description("Clean and disinfect bathroom").frequencyDays(1).build();
-		ChecklistItem bedding2 = ChecklistItem.builder().property(property2).description("Change bed linens")
-				.frequencyDays(1).build();
-		ChecklistItem supplies2 = ChecklistItem.builder().property(property2)
-				.description("Check if any supplies need to be replaced").frequencyDays(15).build();
-		ChecklistItem hottub = ChecklistItem.builder().property(property2).description("Check water level in hot tub")
-				.frequencyDays(7).build();
-		checklistItemRepository.saveAll(List.of(kitchen2, bathroom2, bedding2, supplies2, hottub));
 
 		// CleaningChecklistItems
 		CleaningChecklistItem cleaning1Kitchen = CleaningChecklistItem.builder().cleaning(cleaning1)
