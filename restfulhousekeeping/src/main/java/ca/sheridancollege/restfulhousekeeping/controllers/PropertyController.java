805f5789468e18f064aac383aed17a6193d4ca15
Sophie Wang <wang.sophie@live.ca>
2026-08-03 13:57:00 -0400

    create new property API works

--- restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/controllers/PropertyController.java
+++ restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/controllers/PropertyController.java
@@ -2,25 +2,32 @@ package ca.sheridancollege.restfulhousekeeping.controllers;
 
 import java.util.List;
 
-import ca.sheridancollege.restfulhousekeeping.models.PropertyResponse;
-import ca.sheridancollege.restfulhousekeeping.services.PropertyResponseService;
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ca.sheridancollege.restfulhousekeeping.beans.Property;
+import ca.sheridancollege.restfulhousekeeping.models.PropertyResponse;
 import ca.sheridancollege.restfulhousekeeping.repositories.PropertyRepository;
+import ca.sheridancollege.restfulhousekeeping.services.PropertyResponseService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/properties")
+@SecurityRequirement(name = "Bearer Authentication")
 public class PropertyController {
 
     private final PropertyRepository propertyRepository;
     private final PropertyResponseService propertyResponseService;
-
-//    public PropertyController(PropertyRepository propertyRepository) {
-//        this.propertyRepository = propertyRepository;
-//    }
 
     @GetMapping
     public List<Property> getAll() {
@@ -40,9 +47,11 @@ public class PropertyController {
         return propertyResponseService.getPropertyByUserId(userId);
     }
 
+    // CREATE new property record
     @PostMapping
-    public Property create(@RequestBody Property property) {
-        return propertyRepository.save(property);
+    public ResponseEntity<PropertyResponse> create(@RequestBody Property property) {
+    	PropertyResponse response = propertyResponseService.createProperty(property);
+    	return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{id}")
--- restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/services/PropertyResponseService.java
+++ restfulhousekeeping/src/main/java/ca/sheridancollege/restfulhousekeeping/services/PropertyResponseService.java
@@ -3,13 +3,16 @@ package ca.sheridancollege.restfulhousekeeping.services;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import ca.sheridancollege.restfulhousekeeping.beans.Property;
-import ca.sheridancollege.restfulhousekeeping.models.PropertyResponse;
-import ca.sheridancollege.restfulhousekeeping.repositories.PropertyRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import ca.sheridancollege.restfulhousekeeping.beans.Property;
 import ca.sheridancollege.restfulhousekeeping.beans.Role;
 import ca.sheridancollege.restfulhousekeeping.beans.User;
+import ca.sheridancollege.restfulhousekeeping.models.PropertyResponse;
+import ca.sheridancollege.restfulhousekeeping.repositories.PropertyRepository;
 import ca.sheridancollege.restfulhousekeeping.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 
@@ -43,6 +46,38 @@ public class PropertyResponseService {
                 .map(this::toPropertyResponse)
                 .toList();
 
+    }
+    
+    // CREATE new property record service
+    @Transactional
+    public PropertyResponse createProperty(Property property) {
+    	property.setId(null);
+    	
+        if (property.getManager() == null || property.getManager().getId() == null) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Manager ID is required"
+            );
+        }
+    	
+    	Long managerId = property.getManager().getId();
+    	User manager = userRepository
+    			.findById(managerId)
+    			.orElseThrow(() -> new ResponseStatusException(
+    					HttpStatus.NOT_FOUND,
+    					"User not found: " + managerId));
+    	if (manager.getRole() != Role.MANAGER) {
+    		throw new ResponseStatusException(
+    				HttpStatus.BAD_REQUEST,
+    				"User " + managerId + " is not a manager");
+    	}
+    	
+    	property.setManager(manager);
+    	Property savedProperty = propertyRepository.save(property);
+    	
+    	return toPropertyResponse(savedProperty);
+    			
+    	
     }
 
     private PropertyResponse toPropertyResponse(Property property) {
